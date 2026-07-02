26d398664e98ee2359b4d1e5f9539174d8dababe
ndemanou237 <ndemanoudonald9@gmail.com>
2026-07-02 11:16:50 +0100

    feat: complete weather app project

--- weather-app/script.js
+++ weather-app/script.js
@@ -1,19 +1,61 @@
-const city = document.getElementById('city').value.trim()
-const search = document.getElementById('search')
-const result = document.getElementById('result')
-const stop = document.getElementById('stop')
 
-search.addEventListener('click', () =>{
-    result.innerHTML = `
-    <div class="information">
-        <p>vile</p>
-        <p>temperature</p>
-    </div>
-    <div class="file">
-        <p>image</p>
-    </div>
-    <div class="description">
-        <p>description</p>
-    </div>
-    `
-})
+
+const apiKey = "8a2fc3a00a2d9e2bb034190717c689a9";
+
+
+const search = document.getElementById("search");
+const result = document.getElementById("result");
+
+search.addEventListener("click", async () => {
+    
+    // alert('test')
+    const city = document.getElementById("city").value.trim();
+
+    if (!city) {
+        result.innerHTML = "<p>Veuillez entrer une ville.</p>";
+        return;
+    }
+
+    try {
+        result.innerHTML = "<p>loading...</p>";
+
+        const response = await fetch(
+            `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric&lang=fr`
+        );
+
+        const data = await response.json();
+
+        if (data.cod !== 200) {
+            result.innerHTML = `<p>Ville introuvable.</p>`;
+            return;
+        }
+
+        const temperature = data.main.temp;
+        const description = data.weather[0].description;
+
+        
+
+        const iconCode = data.weather[0].icon;
+        const iconUrl = `http://openweathermap.org/img/wn/${iconCode}@2x.png`;
+
+    
+
+        result.innerHTML = `
+            <div class="information">
+                <p>${city}</p>
+                <p>${temperature}°c</p>
+            </div>
+            <div class="file">
+                <p><img src="${iconUrl}" alt="Météo actuelle"></p>
+            </div>
+            <div class="description">
+                <p>${description}</p>
+            </div>
+            
+            `
+    } 
+    catch (error) {
+        responseDiv.innerHTML = "<p>Une erreur est survenue.</p>";
+        console.error(error);
+    }
+});
